92442b928b471cf06b595fb2287cbc9ea8f2784a
Ondřej Kotas <ok@krtkovo.eu>
2025-09-12 22:59:23 +0200

    career mode files

---
+++ careerPartInventory/ui/ui-vue/src/modules/career/views/PaintingMain.vue
@@ -0,0 +1,22 @@
+<template>
+  <ComputerWrapper :path="[computerStore.computerData.facilityName]" title="Painting" back @back="close">
+    <Painting ref="elPainting" no-header />
+  </ComputerWrapper>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { BngCard } from "@/common/components/base"
+import ComputerWrapper from "./ComputerWrapper.vue"
+import Painting from "../components/painting/Painting.vue"
+import { useComputerStore } from "../stores/computerStore"
+
+const computerStore = useComputerStore()
+
+const elPainting = ref()
+
+const close = () => elPainting.value.close()
+</script>
+
+<style scoped lang="scss">
+</style>
